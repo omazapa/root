d1e538e842ab02477c6eab6341b2d9cef3395477
Omar Zapata <Omar.Zapata@cern.ch>
2023-04-30 13:50:59 -0500

    Fumili: implemented changes https://github.com/root-project/root/compare/master...lmoneta:root:minuit2_fumili_dev [math][minuit2] New Fumili2 version using trust region and dog leg method [math][minuit2] Add support for Fumili scaling the trust region
    - improve also MinimumError to include optionally the Hessian matrix to avoid un-needed inversions

--- math/minuit2/src/FumiliMinimizer.cxx
+++ math/minuit2/src/FumiliMinimizer.cxx
@@ -11,6 +11,7 @@
 #include "Minuit2/FumiliMinimizer.h"
 #include "Minuit2/MinimumSeedGenerator.h"
 #include "Minuit2/FumiliGradientCalculator.h"
+#include "Minuit2/FumiliErrorUpdator.h"
 #include "Minuit2/Numerical2PGradientCalculator.h"
 #include "Minuit2/AnalyticalGradientCalculator.h"
 #include "Minuit2/MinimumBuilder.h"
@@ -40,7 +41,7 @@ FunctionMinimum FumiliMinimizer::Minimize(const FCNBase &fcn, const MnUserParame
    MnPrint print("FumiliMinimizer");
 
    MnUserFcn mfcn(fcn, st.Trafo());
-   Numerical2PGradientCalculator gc(mfcn, st.Trafo(), strategy);
+   //Numerical2PGradientCalculator gc(mfcn, st.Trafo(), strategy);
 
    unsigned int npar = st.VariableParameters();
    if (maxfcn == 0)
@@ -48,7 +49,9 @@ FunctionMinimum FumiliMinimizer::Minimize(const FCNBase &fcn, const MnUserParame
    // FUMILI needs much less function calls
    maxfcn = int(0.1 * maxfcn);
 
-   MinimumSeed mnseeds = SeedGenerator()(mfcn, gc, st, strategy);
+
+   // MinimumSeed mnseeds = SeedGenerator()(mfcn, gc, st, strategy);
+
 
    // downcast fcn
 
@@ -57,11 +60,23 @@ FunctionMinimum FumiliMinimizer::Minimize(const FCNBase &fcn, const MnUserParame
    FumiliFCNBase *fumiliFcn = dynamic_cast<FumiliFCNBase *>(const_cast<FCNBase *>(&fcn));
    if (!fumiliFcn) {
       print.Error("Wrong FCN type; try to use default minimizer");
-      return FunctionMinimum(mnseeds, fcn.Up());
+      return FunctionMinimum(MinimumSeed(MinimumState(0), st.Trafo()), fcn.Up());
    }
 
    FumiliGradientCalculator fgc(*fumiliFcn, st.Trafo(), npar);
-   print.Debug("Using FumiliMinimizer");
+   print.Debug("Using FumiliMinimizer with numerical gradients");
+
+   // compute initial values;
+   const unsigned int n = st.VariableParameters();
+   MnAlgebraicVector x(n);
+   for (unsigned int i = 0; i < n; i++)
+      x(i) = st.IntParameters()[i];
+   double fcnmin = mfcn(x);
+   MinimumParameters pa(x, fcnmin);
+   FunctionGradient grad = fgc(pa);
+   FumiliErrorUpdator errUpdator;
+   MinimumError err = errUpdator.Update(MinimumState(0), pa, fgc, 0.);
+   MinimumSeed mnseeds(MinimumState(pa, err, grad, -1., 1), st.Trafo());
 
    return ModularFunctionMinimizer::Minimize(mfcn, fgc, mnseeds, strategy, maxfcn, toler);
 }
@@ -83,18 +98,30 @@ FunctionMinimum FumiliMinimizer::Minimize(const FCNGradientBase &fcn, const MnUs
    if (maxfcn == 0)
       maxfcn = 200 + 100 * npar + 5 * npar * npar;
 
-   MinimumSeed mnseeds = SeedGenerator()(mfcn, gc, st, strategy);
+   //MinimumSeed mnseeds = SeedGenerator()(mfcn, gc, st, strategy);
 
    // downcast fcn
 
    FumiliFCNBase *fumiliFcn = dynamic_cast<FumiliFCNBase *>(const_cast<FCNGradientBase *>(&fcn));
    if (!fumiliFcn) {
       print.Error("Wrong FCN type; try to use default minimizer");
-      return FunctionMinimum(mnseeds, fcn.Up());
+      return FunctionMinimum(MinimumSeed(MinimumState(0), st.Trafo()), fcn.Up());
    }
 
    FumiliGradientCalculator fgc(*fumiliFcn, st.Trafo(), npar);
-   print.Debug("Using FumiliMinimizer");
+   print.Debug("Using FumiliMinimizer with analytical gradients");
+
+   // compute initial values;
+   const unsigned int n = st.VariableParameters();
+   MnAlgebraicVector x(n);
+   for (unsigned int i = 0; i < n; i++)
+      x(i) = st.IntParameters()[i];
+   double fcnmin = mfcn(x);
+   MinimumParameters pa(x, fcnmin);
+   FunctionGradient grad = fgc(pa);
+   FumiliErrorUpdator errUpdator;
+   MinimumError err = errUpdator.Update(MinimumState(0), pa, fgc, 0.);
+   MinimumSeed mnseeds(MinimumState(pa, err, grad, 1.E10, 1), st.Trafo());
 
    return ModularFunctionMinimizer::Minimize(mfcn, fgc, mnseeds, strategy, maxfcn, toler);
 }
